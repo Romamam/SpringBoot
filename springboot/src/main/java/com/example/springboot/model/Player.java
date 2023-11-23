2dfdcfb32f4832e31a29a43d2efc8dc12091dfdc
Romamam <roman.ribachok@gmail.com>
2023-11-23 20:23:46 +0200

    added usage lombok

--- springboot/src/main/java/com/example/springboot/model/Player.java
+++ springboot/src/main/java/com/example/springboot/model/Player.java
@@ -1,61 +1,22 @@
 package com.example.springboot.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 
 @Entity
 @Table(name = "players")
+@Data
 public class Player {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
     private int id;
-    @Column
+    @Column(name = "name")
     private String name;
-    @Column
+    @Column(name = "second_name")
     private String secondName;
-    @Column
+    @Column(name = "rating")
     private int rating;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    @Override
-    public String toString() {
-        return "Player{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", secondName='" + secondName + '\'' +
-                ", rating=" + rating +
-                '}';
-    }
 }
