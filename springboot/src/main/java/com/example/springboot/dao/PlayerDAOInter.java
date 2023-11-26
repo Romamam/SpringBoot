4a59667c440bce5698287ef69e5dff34bdc928bd
Romamam <roman.ribachok@gmail.com>
2023-11-26 20:01:08 +0200

    added extends PagingAndSortingRepository

--- springboot/src/main/java/com/example/springboot/dao/PlayerDAOInter.java
+++ springboot/src/main/java/com/example/springboot/dao/PlayerDAOInter.java
@@ -2,6 +2,13 @@ package com.example.springboot.dao;
 
 import com.example.springboot.model.Player;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface PlayerDAOInter extends CrudRepository<Player, Integer> {
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PlayerDAOInter extends PagingAndSortingRepository<Player, Integer>, CrudRepository<Player, Integer> {
+    List<Player> findByName(@Param("name") String name);
 }
