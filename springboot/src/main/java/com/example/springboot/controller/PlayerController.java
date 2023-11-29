316d7b2726b2012f7375347e7811e4b9208698e1
Romamam <roman.ribachok@gmail.com>
2023-11-29 16:33:42 +0200

    transferred methods to TeamController and TeamService

--- springboot/src/main/java/com/example/springboot/controller/PlayerController.java
+++ springboot/src/main/java/com/example/springboot/controller/PlayerController.java
@@ -1,19 +1,15 @@
 package com.example.springboot.controller;
 
-import com.example.springboot.dao.PlayerDAOInter;
 import com.example.springboot.model.Player;
 import com.example.springboot.service.PlayerService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class PlayerController {
 
-    private final Map<Integer, List<Player>> generatedTeamsMap = new HashMap<>();
     private final PlayerService playerService;
 
     public PlayerController(PlayerService playerService) {
@@ -46,16 +42,4 @@ public class PlayerController {
         return playerService.updatePlayer(playerObj);
     }
 
-
-    @GetMapping("/generated-teams")
-    public List<List<Player>> generateTeamsWithBalancedRating() {
-        return playerService.generateTeamsWithBalancedRating();
-    }
-
-//    @GetMapping("/get-saved-teams/{teamId}")
-//    public ResponseEntity<List<Player>> getSavedTeams(@PathVariable int teamId) {
-//        Optional<List<Player>> team = Optional.ofNullable(generatedTeamsMap.get(teamId));
-//
-//        return team.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-//    }
 }
--- springboot/src/main/java/com/example/springboot/service/PlayerService.java
+++ springboot/src/main/java/com/example/springboot/service/PlayerService.java
@@ -2,16 +2,13 @@ package com.example.springboot.service;
 
 import com.example.springboot.dao.PlayerDAOInter;
 import com.example.springboot.model.Player;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class PlayerService {
 
-    private final Map<Integer, List<Player>> generatedTeamsMap = new HashMap<>();
     private final PlayerDAOInter playerDAO;
 
     public PlayerService(PlayerDAOInter playerDAO) {
@@ -46,66 +43,8 @@ public class PlayerService {
         return playerDAO.save(player);
     }
 
-    public List<List<Player>> generateTeamsWithBalancedRating(){
-        List<Player> allPlayers = listOfPlayers();
-
-        if (allPlayers.size() < 22) {
-            System.out.println("There is no required number of players to create teams");
-            return Collections.emptyList();
-        }
-
-        List<Player> selectedPlayers = getRandomPlayers(allPlayers, 22);
-
-        List<Player> team1 = new ArrayList<>();
-        List<Player> team2 = new ArrayList<>();
-
-        for (int i = 0; i < selectedPlayers.size(); i++) {
-            if (i % 2 == 0) {
-                team1.add(selectedPlayers.get(i));
-            } else {
-                team2.add(selectedPlayers.get(i));
-            }
-        }
-
-        int team1Rating = calculateTeamRating(team1);
-        int team2Rating = calculateTeamRating(team2);
-
-        while (Math.abs(team1Rating - team2Rating) > 20) {
-            Collections.shuffle(selectedPlayers);
-
-            team1 = new ArrayList<>();
-            team2 = new ArrayList<>();
-            for (int i = 0; i < selectedPlayers.size(); i++) {
-                if (i % 2 == 0) {
-                    team1.add(selectedPlayers.get(i));
-                } else {
-                    team2.add(selectedPlayers.get(i));
-                }
-            }
-
-            team1Rating = calculateTeamRating(team1);
-            team2Rating = calculateTeamRating(team2);
-        }
-
-        System.out.println("team1: " + team1Rating/11);
-        System.out.println("team2: " + team2Rating/11);
-        List<List<Player>> result = new ArrayList<>();
-        result.add(team1);
-        result.add(team2);
-
-        generatedTeamsMap.put(1, team1);
-        generatedTeamsMap.put(2, team2);
-
-        return result;
-    }
-
-    private List<Player> getRandomPlayers(List<Player> allPlayers, int count) {
-        Collections.shuffle(allPlayers);
-        return allPlayers.stream().limit(count).collect(Collectors.toList());
-    }
-
-    private int calculateTeamRating(List<Player> team) {
-        return team.stream().mapToInt(Player::getRating).sum();
+    public void savePlayers(List<Player> players) {
+        playerDAO.saveAll(players);
     }
 
 }
