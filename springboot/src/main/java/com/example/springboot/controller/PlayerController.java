bb84deb02cdb8d0324b3169e5f317e18c782759e
Romamam <roman.ribachok@gmail.com>
2023-11-30 11:55:38 +0200

    changed endpoint /players for sorting and paging

--- springboot/src/main/java/com/example/springboot/controller/PlayerController.java
+++ springboot/src/main/java/com/example/springboot/controller/PlayerController.java
@@ -2,6 +2,10 @@ package com.example.springboot.controller;
 
 import com.example.springboot.model.Player;
 import com.example.springboot.service.PlayerService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -17,8 +21,10 @@ public class PlayerController {
     }
 
     @GetMapping("/players")
-    public List<Player> get(){
-        return playerService.listOfPlayers();
+    public Page<Player> get(Pageable pageable){
+        Sort sortByRatingDescending = Sort.by("rating").descending();
+        Pageable pageableWithSort = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sortByRatingDescending);
+        return playerService.listOfPlayers(pageableWithSort);
     }
 
     @PostMapping("/player")
