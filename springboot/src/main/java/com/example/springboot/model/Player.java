37edc4763bf8246cc97d3797d8c2ced2cb35e11d
Romamam <roman.ribachok@gmail.com>
2023-12-15 00:06:45 +0200

    changed imports

--- springboot/src/main/java/com/example/springboot/model/Player.java
+++ springboot/src/main/java/com/example/springboot/model/Player.java
@@ -1,10 +1,12 @@
 package com.example.springboot.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
