73ab0a8754d182b71f86f49342a8e480bc1c2dce
Romamam <roman.ribachok@gmail.com>
2023-12-20 18:03:39 +0200

    removed unnecessary methods

--- springboot/src/main/java/com/example/springboot/controller/PlayerController.java
+++ springboot/src/main/java/com/example/springboot/controller/PlayerController.java
@@ -1,13 +1,10 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.dao.PlayerRepository;
-import com.example.springboot.model.Player;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,10 +32,5 @@ public class PlayerController {
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Player with ID " + id + " not found");
         }
-    }
-
-    @DeleteMapping("/deleteAll")
-    public void deleteAll(){
-        playerRepository.deleteAll();
     }
 }
